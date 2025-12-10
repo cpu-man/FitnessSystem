3abfae163060706dcf84dd13f79053862858f92f
Philip Bryde <brydephilip@gmail.com>
2025-12-10 10:01:40 +0100

    Very minor margin change

--- FitnessProgram/obj/Debug/net8.0-windows/App.g.i.cs
+++ FitnessProgram/obj/Debug/net8.0-windows/App.g.i.cs
@@ -47,7 +47,7 @@ namespace FitnessProgram {
         /// InitializeComponent
         /// </summary>
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.1.0")]
         public void InitializeComponent() {
             if (_contentLoaded) {
                 return;
@@ -73,7 +73,7 @@ namespace FitnessProgram {
         /// </summary>
         [System.STAThreadAttribute()]
         [System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.0.0")]
+        [System.CodeDom.Compiler.GeneratedCodeAttribute("PresentationBuildTasks", "10.0.1.0")]
         public static void Main() {
             FitnessProgram.App app = new FitnessProgram.App();
             app.InitializeComponent();
